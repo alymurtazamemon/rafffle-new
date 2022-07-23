e752d3c9028cfc7f1a8fed0b50dc0ec043f22c8d
alithegilfoyle <alimurtaza-assistant@outlook.com>
2022-07-23 08:06:17 +0500

    fix issuess

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,20 @@
 //ensa hal jemle
 //backend js is a little different than frontend JS
 
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
 // import ManualHeader from "../components/ManualHeader"
-import { Header } from "../components/Header";
+import Header from "../components/Header"
+
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Smart Contract Lottery</title>
-        <meta name="description" content="Our Smart Sontract Lottery" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header></Header>
-      Hello!!
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Smart Contract Lottery</title>
+                <meta name="description" content="Our Smart Sontract Lottery" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+        </div>
+    )
 }
